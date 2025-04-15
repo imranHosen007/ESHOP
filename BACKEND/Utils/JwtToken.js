9c6cc961555936805a76d39cc12ec42f16c37e95
imranHosen007 <imrangit007@gmail.com>
2025-04-15 16:09:25 +0600

    fixing-jwt-bug

--- BACKEND/Utils/JwtToken.js
+++ BACKEND/Utils/JwtToken.js
@@ -5,7 +5,7 @@ export const sendToken = (user, statusCode, res) => {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     httpOnly: true,
     secure: true,
-    sameSite: none,
+    sameSite: "none",
   };
   res.status(statusCode).cookie("token", token, options).json({
     success: true,
